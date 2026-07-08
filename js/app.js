99d7ad757e369563b96456384938419453d71f13
yigitcanevcan134-svg <yigitcanevcan134@gmail.com>
2026-07-08 22:06:13 +0300

    Implement shopping cart functions in app.js

--- js/app.js
+++ js/app.js
@@ -1,1 +1,27 @@
 
+// Y&M WEAR
+
+let cart = JSON.parse(localStorage.getItem("cart")) || [];
+
+function addToCart(name, price) {
+    cart.push({
+        name: name,
+        price: price
+    });
+
+    localStorage.setItem("cart", JSON.stringify(cart));
+
+    alert(name + " sepete eklendi!");
+}
+
+function removeFromCart(index) {
+    cart.splice(index, 1);
+    localStorage.setItem("cart", JSON.stringify(cart));
+}
+
+function getCartCount() {
+    return cart.length;
+}
+
+console.log("Y&M WEAR yüklendi.");
+console.log("Sepette " + getCartCount() + " ürün var.");
