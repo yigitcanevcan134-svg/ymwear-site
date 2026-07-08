c14d5abdb494c839323d2a8caee272635cecb7b0
yigitcanevcan134-svg <yigitcanevcan134@gmail.com>
2026-07-08 23:46:41 +0300

    Refactor addToCart function for price handling
    Refactored addToCart function to ensure price is a number and moved cart initialization inside the function.

--- js/app.js
+++ js/app.js
@@ -1,27 +1,6 @@
-
-// Y&M WEAR
-
-let cart = JSON.parse(localStorage.getItem("cart")) || [];
-
-function addToCart(name, price) {
-    cart.push({
-        name: name,
-        price: price
-    });
-
-    localStorage.setItem("cart", JSON.stringify(cart));
-
-    alert(name + " sepete eklendi!");
+function addToCart(name, price){
+  let cart = JSON.parse(localStorage.getItem("cart")) || [];
+  cart.push({name:name, price:Number(price)});
+  localStorage.setItem("cart", JSON.stringify(cart));
+  alert(name + " sepete eklendi!");
 }
-
-function removeFromCart(index) {
-    cart.splice(index, 1);
-    localStorage.setItem("cart", JSON.stringify(cart));
-}
-
-function getCartCount() {
-    return cart.length;
-}
-
-console.log("Y&M WEAR yüklendi.");
-console.log("Sepette " + getCartCount() + " ürün var.");
