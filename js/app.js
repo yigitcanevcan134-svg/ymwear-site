2693391ac932c3bf509847f8147dedce7e5e2ab8
yigitcanevcan134-svg <yigitcanevcan134@gmail.com>
2026-07-09 16:03:44 +0300

    Enhance addToCart function with options for size and color

--- js/app.js
+++ js/app.js
@@ -1,6 +1,19 @@
-function addToCart(name, price){
-  let cart = JSON.parse(localStorage.getItem("cart")) || [];
-  cart.push({name:name, price:Number(price)});
-  localStorage.setItem("cart", JSON.stringify(cart));
-  alert(name + " sepete eklendi!");
+function addToCartWithOptions(button, name, price) {
+    let card = button.closest(".card");
+
+    let beden = card.querySelector(".beden").value;
+    let renk = card.querySelector(".renk").value;
+
+    let cart = JSON.parse(localStorage.getItem("cart")) || [];
+
+    cart.push({
+        name: name,
+        price: Number(price),
+        beden: beden,
+        renk: renk
+    });
+
+    localStorage.setItem("cart", JSON.stringify(cart));
+
+    alert(name + " (" + beden + " - " + renk + ") sepete eklendi!");
 }
